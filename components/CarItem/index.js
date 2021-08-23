a6a9c93361d31dc14166acca38f9c6660a768c42
Ashish Rawat <Ashish.rawat417@gmail.com>
2021-08-24 04:36:45 +1000

    Final commit

---
+++ components/CarItem/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Text, View, ImageBackground } from 'react-native'
+import styles from './style'
+import Button from '../Button'
+
+const CarItem = (props) => {
+    const { name, tagline, taglineCTA, image } = props.car
+    return (
+        <View style={styles.carContainer}>
+            <ImageBackground 
+            source={image} 
+            style={styles.image}
+            />
+            <View style={styles.titles}>
+                <Text style={styles.title}>{name}</Text>
+                <Text style={styles.subtitle}>
+                    {tagline}
+                    {' '}
+                    <Text style={styles.subtitleCTA}>
+                        {taglineCTA}
+                    </Text>
+                </Text>
+            </View>
+            <View style={styles.buttonsContainer}>
+                <Button type="primary" content={"Custom Order"} onPress={() => { console.warn("Primary Button") }}/>
+                <Button type="secondary" content={"Existing Inventory"} onPress={() => { console.warn("Secondary Button") }} />
+            </View>
+      </View>
+    )
+}
+
+export default CarItem
